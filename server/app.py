0cf26fa3cbcd8a84341831ca613cf43d1c69b786
SaaraKhalid <khalidsaara6@gmail.com>
2024-04-15 22:47:46 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -28,16 +28,42 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.filter(User.id == session.get('user_id')).first()
+            if user:
+                return user.to_dict(), 200
+        
+        return {}, 204
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+        
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+
+        session['user_id'] = None
+        
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
